7061b9debf3a52649017d7e0cab67b51aea6e5c5
Mena Song-Lew <msonglew@gmail.com>
2025-05-16 12:33:42 -0700

    Add bike lanes

--- map.js
+++ map.js
@@ -6,10 +6,44 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibXNvbmdsZXciLCJhIjoiY21hcjYzMm54MDM2eDJrcTRnO
 
 // Initialize the map
 const map = new mapboxgl.Map({
-  container: 'map', // ID of the div where the map will render
-  style: 'mapbox://styles/mapbox/streets-v12', // Map style
-  center: [-71.09415, 42.36027], // [longitude, latitude]
-  zoom: 12, // Initial zoom level
-  minZoom: 5, // Minimum allowed zoom
-  maxZoom: 18, // Maximum allowed zoom
+    container: 'map', // ID of the div where the map will render
+    style: 'mapbox://styles/mapbox/streets-v12', // Map style
+    center: [-71.09415, 42.36027], // [longitude, latitude]
+    zoom: 12, // Initial zoom level
+    minZoom: 5, // Minimum allowed zoom
+    maxZoom: 18, // Maximum allowed zoom
 });
+
+let bikeRouteStyle = {
+    'line-color': '#32D400',
+    'line-width': 4,
+    'line-opacity': 0.6,
+};
+
+map.on('load', async () => {
+    //code
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+    });
+
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
+    });
+
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: bikeRouteStyle,
+    });
+
+    map.addLayer({
+        id: 'bike-lanes-cambridge',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: bikeRouteStyle,
+    });
+});
+
