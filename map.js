8e68206a962d5f7ade79675ff5fac8f4d8c57250
Mena Song-Lew <msonglew@gmail.com>
2025-05-16 15:02:48 -0700

    Visualize bike traffic
    Add stations and station data
    Added tooltip

--- map.js
+++ map.js
@@ -30,7 +30,7 @@ function getCoords(station) {
 }
 
 map.on('load', async () => {
-    //code
+    // BIKE ROUTES
     map.addSource('boston_route', {
         type: 'geojson',
         data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
@@ -55,6 +55,7 @@ map.on('load', async () => {
         paint: bikeRouteStyle,
     });
 
+    // BIKE STATIONS
     let jsonData;
     try {
         const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
@@ -65,9 +66,47 @@ map.on('load', async () => {
     } catch (error) {
         console.error('Error loading JSON:', error); // Handle errors
     };
-
     let stations = jsonData.data.stations;
     console.log('Stations Array:', stations);
+
+    // RENTAL BIKE TRIPS
+    let csvData;    
+    try {
+        const csvurl = 'https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv';
+
+    // Await JSON fetch
+        csvData = await d3.csv(csvurl);
+        console.log('Loaded CSV Data:', csvData); // Log to verify structure
+    } catch (error) {
+        console.error('Error loading CSV:', error); // Handle errors
+    };
+    let trips = csvData;
+    console.log('Trips Array:', trips);
+
+    const departures = d3.rollup(
+        trips,
+        (v) => v.length,
+        (d) => d.start_station_id,
+    );
+
+    const arrivals = d3.rollup(
+        trips,
+        (v) => v.length,
+        (d) => d.end_station_id,
+    );
+
+    stations = stations.map((station) => {
+        let id = station.short_name;
+        station.arrivals = arrivals.get(id) ?? 0;
+        station.departures = departures.get(id) ?? 0;
+        station.totalTraffic = station.arrivals + station.departures;
+        return station;
+    });
+
+    const radiusScale = d3
+    .scaleSqrt()
+    .domain([0, d3.max(stations, (d) => d.totalTraffic)])
+    .range([0, 25]);
 
     const svg = d3.select('#map').append('svg');
 
@@ -76,11 +115,18 @@ map.on('load', async () => {
         .data(stations)
         .enter()
         .append('circle')
-        .attr('r', 5) // Radius of the circle
+        .attr('r', (d) => radiusScale(d.totalTraffic)) // Radius of the circle
         .attr('fill', 'steelblue') // Circle fill color
         .attr('stroke', 'white') // Circle border color
         .attr('stroke-width', 1) // Circle border thickness
-        .attr('opacity', 0.8); // Circle opacity
+        .attr('opacity', 0.8) // Circle opacity
+        .each(function (d) {
+            // Add <title> for browser tooltips
+            d3.select(this)
+            .append('title')
+            .text(
+                `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`,
+        )});
 
     // Function to update circle positions when the map moves/zooms
     function updatePositions() {
